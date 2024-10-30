1e29f2d0231048b82f5427ad9a20127cd9815e71
Roberto Griel Filho <roberto@griel.com.br>
2024-10-30 15:13:52 -0300

    Enhancing update section with replication tag; Add types to methods

--- src/Console/Commands/StartReplicationCommand.php
+++ src/Console/Commands/StartReplicationCommand.php
@@ -7,7 +7,11 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use MySQLReplication\Config\ConfigBuilder;
 use MySQLReplication\Definitions\ConstEventType;
+use MySQLReplication\Event\DTO\DeleteRowsDTO;
 use MySQLReplication\Event\DTO\EventDTO;
+use MySQLReplication\Event\DTO\UpdateRowsDTO;
+use MySQLReplication\Event\DTO\WriteRowsDTO;
+use MySQLReplication\Event\Event;
 use MySQLReplication\Event\EventSubscribers;
 use MySQLReplication\MySQLReplicationFactory;
 
@@ -17,7 +21,7 @@ class StartReplicationCommand extends Command
     protected $description = 'Inicia o processo de replicação configurado no pacote Replicator';
     public const CACHE_ULTIMA_ALTERACAO = 'replicador.colaborador_loja.ultima_alteracao';
 
-    public function handle()
+    public function handle(): void
     {
         $configurations = config('replicator');
 
@@ -37,15 +41,20 @@ class StartReplicationCommand extends Command
         $registro = new class($configurations) extends EventSubscribers {
             private array $configurations;
 
-            public function __construct($configurations)
+            public function __construct(array $configurations)
             {
                 $this->configurations = $configurations;
             }
 
             public function allEvents(EventDTO $event): void
             {
-                $database = $event->getTableMap()->getDatabase();
-                $table = $event->getTableMap()->getTable();
+
+                if (!($event instanceof WriteRowsDTO || $event instanceof UpdateRowsDTO || $event instanceof DeleteRowsDTO)) {
+                    return;
+                }
+
+                $database = $event->tableMap->database;
+                $table = $event->tableMap->table;
 
                 foreach ($this->configurations as $config) {
                     $sourceDatabase = $config['database'];
@@ -57,72 +66,80 @@ class StartReplicationCommand extends Command
                         ($database === $sourceDatabase && $table === $sourceTable) ||
                         ($database === $targetDatabase && $table === $targetTable)
                     ) {
-                        $this->replicateEvent($event, $config, $database, $table);
-                        break;
-                    }
-                }
-            }
 
-            private function replicateEvent(EventDTO $event, $config, $eventDatabase, $eventTable): void
-            {
-                if ($eventDatabase === $config['database'] && $eventTable === $config['table']) {
-                    $sourceConfig = $config;
-                    $targetConfig = $config['sync'];
-                    $columnMappings = $config['sync']['columns'];
-                } else {
-                    $sourceConfig = $config['sync'];
-                    $targetConfig = $config;
-                    $columnMappings = array_flip($config['sync']['columns']);
-                }
+                        if (
+                            $event->tableMap->database === $config['database'] &&
+                            $event->tableMap->table === $config['table']
+                        ) {
+                            $sourceConfig = $config;
+                            $targetConfig = $config['sync'];
+                            $columnMappings = $config['sync']['columns'];
+                        } else {
+                            $sourceConfig = $config['sync'];
+                            $targetConfig = $config;
+                            $columnMappings = array_flip($config['sync']['columns']);
+                        }
 
-                $sourcePrimaryKey = $sourceConfig['primary_key'];
-                $targetDatabase = $targetConfig['database'];
-                $targetTable = $targetConfig['table'];
-                $targetPrimaryKey = $targetConfig['primary_key'];
+                        $sourcePrimaryKey = $sourceConfig['reference_key'];
+                        $targetDatabase = $targetConfig['database'];
+                        $targetTable = $targetConfig['table'];
+                        $targetPrimaryKey = $targetConfig['reference_key'];
 
-                foreach ($event->getValues() as $row) {
-                    $before = $row['before'] ?? [];
-                    $after = $row['after'] ?? [];
+                        foreach ($event->values as $row) {
 
-                    $eventType = $event->getType(); // 'update', 'write', 'delete'
+                            switch ($event::class) {
+                                case UpdateRowsDTO::class:
 
-                    switch ($eventType) {
-                        case 'update':
-                            $this->handleUpdate(
-                                $sourcePrimaryKey,
-                                $targetDatabase,
-                                $targetTable,
-                                $targetPrimaryKey,
-                                $columnMappings,
-                                $before,
-                                $after
-                            );
-                            break;
+                                    $before = $row['before'];
+                                    $after = $row['after'];
 
-                        case 'write':
+                                    $this->handleUpdate(
+                                        $sourcePrimaryKey,
+                                        $targetDatabase,
+                                        $targetTable,
+                                        $targetPrimaryKey,
+                                        $columnMappings,
+                                        $before,
+                                        $after
+                                    );
+                                    break;
 
-                            break;
+                                case WriteRowsDTO::class:
+                                    $this->handleInsert(
+                                        $targetDatabase,
+                                        $targetTable,
+                                        $row
+                                    );
+                                    break;
 
-                        case 'delete':
-
-                            break;
+                                case DeleteRowsDTO::class:
+                                    $this->handleDelete(
+                                        $targetDatabase,
+                                        $targetTable,
+                                        $targetPrimaryKey,
+                                        $row
+                                    );
+                                    break;
+                            }
+                        }
                     }
                 }
             }
 
             private function handleUpdate(
-                $sourcePrimaryKey,
-                $targetDatabase,
-                $targetTable,
-                $targetPrimaryKey,
-                $columnMappings,
-                $before,
-                $after
+                string $sourcePrimaryKey,
+                string $targetDatabase,
+                string $targetTable,
+                string $targetPrimaryKey,
+                array  $columnMappings,
+                array  $before,
+                array  $after
             ): void
             {
                 $changedColumns = [];
                 foreach ($columnMappings as $sourceColumn => $targetColumn) {
-                    if ($before[$sourceColumn] !== $after[$sourceColumn]) {
+
+                    if (array_diff_assoc($before, $after)) {
                         $changedColumns[$targetColumn] = $after[$sourceColumn];
                     }
                 }
@@ -130,14 +147,48 @@ class StartReplicationCommand extends Command
                 if (!empty($changedColumns)) {
                     $primaryKeyValue = $after[$sourcePrimaryKey];
 
+                    $binds = [];
+                    foreach ($changedColumns as $column => $value) {
+                        $binds[":{$column}"] = $value;
+                    }
+                    $binds[":{$targetPrimaryKey}"] = $primaryKeyValue;
+
+                    $replicateTag = Event::REPLICATION_QUERY;
+
+                    DB::update("UPDATE {$targetDatabase}.{$targetTable}
+                        SET " . implode(', ', array_map(function ($column) use ($targetDatabase, $targetTable) {
+                            return "{$targetDatabase}.{$targetTable}.{$column} = :{$column}";
+                        }, array_keys($changedColumns))) . "
+                        WHERE {$targetDatabase}.{$targetTable}.{$targetPrimaryKey} = :{$targetPrimaryKey} {$replicateTag};",
+                        $binds);
+                }
+            }
+
+            private function handleInsert(
+                string $targetDatabase,
+                string $targetTable,
+                array  $data
+            ): void
+            {
+                if (!empty($data)) {
                     DB::statement('SET SESSION sql_log_bin=0;');
 
                     DB::table("{$targetDatabase}.{$targetTable}")
-                        ->where($targetPrimaryKey, $primaryKeyValue)
-                        ->update($changedColumns);
+                        ->insert($data);
 
                     DB::statement('SET SESSION sql_log_bin=1;');
                 }
+            }
+
+            private function handleDelete(string $targetDatabase, string $targetTable, string $targetPrimaryKey, array $data): void
+            {
+
+                $primaryKeyValue = $data[$targetPrimaryKey];
+
+                DB::table("{$targetDatabase}.{$targetTable}")
+                    ->where($targetPrimaryKey, $primaryKeyValue)
+                    ->delete();
+
             }
         };
 
@@ -149,7 +200,8 @@ class StartReplicationCommand extends Command
             ->withEventsOnly([
                 ConstEventType::UPDATE_ROWS_EVENT_V1,
                 ConstEventType::WRITE_ROWS_EVENT_V1,
-                ConstEventType::DELETE_ROWS_EVENT_V1
+                ConstEventType::DELETE_ROWS_EVENT_V1,
+                ConstEventType::MARIA_ANNOTATE_ROWS_EVENT
             ])
             ->withDatabasesOnly($databases)
             ->withTablesOnly($tables);
